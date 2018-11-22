0b39410d52ee0662a629eb6977fe5275332c04de
viniciusmdalmeida <viniciusmdea@gmail.com>
2018-11-22 17:17:31 -0200

    Versão Extra: 1.4

--- Projeto/BancoDados.py
+++ Projeto/BancoDados.py
@@ -1,6 +1,24 @@
-import MySQLdb
+import mysql.connector
 import pandas as pd
 #Instalar pip install MySQL-python
+
+class conectar:
+    def __enter__(self):
+        host = 'db4free.net'
+        database = 'desafiostone'
+        user = 'desafiostone'
+        password = 'stoneQ123'
+        port = 3306
+
+        # Conectando ao banco
+        self.con = mysql.connector.connect(user=user,
+                                      password=password,
+                                      host=host,
+                                      database=database)
+        return self.con
+
+    def __exit__(self, type, value, traceback):
+        self.con.close()
 
 class BancoDados:
     host = 'sql10.freemysqlhosting.net'
@@ -15,48 +33,50 @@ class BancoDados:
         except:
             pass
 
-    def conectar(self):
-        self.con = MySQLdb.connect(self.host, self.user, self.password)
-        self.con.select_db(self.database)
-        self.cursor = self.con.cursor()
-
     def criarTabelas(self):
-        self.conectar()
-        try:
+        with conectar() as con:
+            cursor = con.cursor()
             sql = "CREATE TABLE companhias (" \
-                  " cnpj VARCHAR(20) NOT NULL," \
-                  " nome VARCHAR(100) NOT NULL," \
-                  " atividade_principal VARCHAR(100)," \
-                  " data_de_abertura DATETIME ," \
-                  " capital_social DOUBLE," \
-                  " PRIMARY KEY (cnpj));"
-            self.cursor.execute(sql)
+              " cnpj VARCHAR(20) NOT NULL," \
+              " nome VARCHAR(100) NOT NULL," \
+              " atividade_principal VARCHAR(100)," \
+              " data_de_abertura DATETIME ," \
+              " capital_social DOUBLE," \
+              " bairro VARCHAR(100)," \
+              " cep VARCHAR(50)," \
+              " complemento VARCHAR(100)," \
+              " numero INTEGER," \
+              " logradouro VARCHAR(100)," \
+              " municipio VARCHAR(100)," \
+              " data_situacao DATETIME ," \
+              " natureza_juridica VARCHAR(100)," \
+              " telefone VARCHAR(20)," \
+              " tipo VARCHAR(20)," \
+              " uf VARCHAR(20)," \
+              " email VARCHAR(100),"\
+              " fantasia VARCHAR(100)," \
+              " PRIMARY KEY (cnpj));"
+            cursor.execute(sql)
 
             sql = "CREATE TABLE QSA (" \
                   " nome VARCHAR(100)," \
                   " qual VARCHAR(100) NOT NULL," \
                   " cnpj VARCHAR(20));"
-            self.cursor.execute(sql)
-            self.con.close()
-        except Exception as  e:
-            print("Error",e)
-            self.con.close()
+            cursor.execute(sql)
+            con.close()
+
 
     def inserirCompanhia(self,linha):
-        self.conectar()
-        try:
+        with conectar() as con:
+            cursor = con.cursor()
             sql = "INSERT INTO companhias (cnpj,nome,atividade_principal,data_de_abertura,capital_social)  VALUES (%s,%s,%s,%s,%s)"
             val = (linha['cnpj'], linha['nome'], linha['atividade_principal'], linha['abertura'], linha['capital_social'])
-            self.cursor.execute(sql, val)
-            self.con.commit()
-        except  Exception as e:
-            print('error:', e)
-        else:
-            self.con.close()
+            cursor.execute(sql, val)
+            con.commit()
 
     def inserirCompanhia2(self,linha):
-        self.conectar()
-        try:
+        with conectar() as con:
+            cursor = con.cursor()
             sql = "INSERT INTO companhias (cnpj,nome,atividade_principal,abertura,capital_social,bairro,cep,"
             "complemento,numero,logradouro,municipio,data_situacao,natureza_juridica,telefone,tipo,uf,email,fantasia)" \
             "  VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
@@ -67,45 +87,30 @@ class BancoDados:
                 str(linha['logradouro']), str(linha['municipio']),
                 str(linha['data_situacao']), str(linha['natureza_juridica']), str(linha['telefone']),
                 str(linha['tipo']), str(linha['uf']), str(linha['email']), str(linha['fantasia']))
-            self.cursor.execute(sql, val)
-            self.con.commit()
-        except  Exception as e:
-            print('error:', e)
-        else:
-            self.con.close()
+            cursor.execute(sql, val)
+            con.commit()
 
     def inserirQSA(self,linha):
-        self.conectar()
-        try:
+        with conectar() as con:
+            cursor = con.cursor()
             sql = "INSERT INTO QSA (nome,qual,cnpj)  VALUES (%s,%s,%s)"
-            val = (linha['nome'], linha['qual'], linha['CNPJ'])
+            val = (linha['nome'], linha['qual'], str(linha['CNPJ']))
             print(val)
-            self.cursor.execute(sql, val)
-            self.con.commit()
-        except  Exception as e:
-            print('error:', e)
-        else:
-            self.con.close()
+            cursor.execute(sql, val)
+            con.commit()
 
     def pegarCompanhia(self):
-        self.conectar()
-        try:
-            tabela = pd.read_sql('SELECT * FROM companhias', con=self.con)
-            self.con.close()
+        with conectar() as con:
+            tabela = pd.read_sql('SELECT * FROM companhias', con=con)
+            con.close()
             return tabela
-        except:
-            self.con.close()
 
 
     def pegarQSA(self):
-        self.conectar()
-        try:
-            tabela = pd.read_sql('SELECT * FROM QSA', con=self.con)
-            self.con.close()
+        with conectar() as con:
+            tabela = pd.read_sql('SELECT * FROM QSA', con=con)
+            con.close()
             return tabela
-        except:
-            self.con.close()
-        return tabela
 
     def escreverCompanhias(self,tabela):
         for n_linha in range(len(tabela)):
--- Projeto/Interface.py
+++ Projeto/Interface.py
@@ -33,3 +33,5 @@ class TelaInial():
 
 
         master.mainloop()
+
+TelaInial()
